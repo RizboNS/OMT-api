5dc656f4790256df156a42c3adc9919275baf8f1
rizbons <rizbo.ns.bk@gmail.com>
2022-10-03 11:14:03 +0200

    stockingLocation MODEL >TD router&Controller

---
+++ functions/models/stockingLocation.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const stockingLocationSchema = new Schema({
+  address: {
+    type: String,
+    reqruired: true,
+    max: 255,
+  },
+  city: {
+    type: String,
+    required: true,
+    max: 255,
+  },
+  state: {
+    type: String,
+    required: true,
+    max: 255,
+  },
+  zip: {
+    type: String,
+    required: true,
+    max: 5,
+    min: 5,
+  },
+  productsInStock: [
+    {
+      product: {
+        type: Schema.Types.ObjectId,
+        ref: "product",
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+});
+
+const StockingLocation = mongoose.model(
+  "stockingLocation",
+  stockingLocationSchema
+);
+module.exports = StockingLocation;
